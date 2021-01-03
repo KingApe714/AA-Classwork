8a451ea761c1c70419ee6e77323a46b38e8bc3fd
jonathandiaz <jonathan.diaz714@gmail.com>
2021-01-02 19:12:41 -0500

    finished part 7

--- SQL_Zoo/lib/07_joins_exercises.rb
+++ SQL_Zoo/lib/07_joins_exercises.rb
@@ -167,6 +167,13 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
+    SELECT
+      title
+    FROM
+      movies
+    WHERE
+      yr = 1978
+    
   SQL
 end
 
