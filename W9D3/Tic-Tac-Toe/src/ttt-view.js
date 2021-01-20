e3d9cb555ccc9729cb304689546d70f111c1999e
Trevor Smith <trevor.smith1667@gmail.com>
2021-01-20 12:20:42 -0800

    added view and game

--- W9D3/Tic-Tac-Toe/src/ttt-view.js
+++ W9D3/Tic-Tac-Toe/src/ttt-view.js
@@ -6,7 +6,14 @@ class View {
   makeMove($square) {}
 
   setupBoard() {
-    
+    const $ul = $("<ul>")
+    for(let i = 0; i < 3; i++){
+      for(let j = 0; j < 3; j++){
+      //let $li = $('<li>')
+        $ul[i][j] = $("li")
+      // $ul.append($li)
+      }
+    }
   }
 }
 
