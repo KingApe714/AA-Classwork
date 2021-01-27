c2f6744f0e1982656aa767f39379fa5ada275ab4
jonathandiaz <jonathan.diaz714@gmail.com>
2021-01-27 17:21:59 -0500

    finished phase 3

--- W10D3/minesweeper/components/tile.jsx
+++ W10D3/minesweeper/components/tile.jsx
@@ -10,15 +10,32 @@ class Tile extends React.Component {
 
       // when clicked -> change the underlying Tile through updateGame
       // 
+      this.handleClick = this.handleClick.bind(this);
     }
+
+    handleClick(e){
+      e.preventDefault();
+      this.props.updateGame(this.props.tile, e.altKey);
+    }
+
     render() {
       //className for bombed, explored, flagged - 
       //generate that string based on Tile's properties
-      debugger
+      // debugger
+      let content = "";
+      let status = "";
+      if (this.props.tile.bombed && this.props.tile.explored) {
+        status = "bombed"
+        content = "☢"
+      } else if (this.props.tile.explored) {
+        status = "explored"
+        content = this.props.tile.adjacentBombCount()
+      } else if (this.props.tile.flagged) {
+        status = "flagged"
+        content = "⚑"
+      }
       return (
-        <>
-          <div className="tile"></div>
-        </>
+        <div className={`${status} tile`} onClick={this.handleClick}>{content}</div>
       )
     }
   }
