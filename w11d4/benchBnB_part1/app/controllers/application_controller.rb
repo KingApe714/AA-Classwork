7dc1cdb55c2cc457c9f902829fdec07e686da846
jonathandiaz <jonathan.diaz714@gmail.com>
2021-02-04 21:49:45 -0500

    finshed backend for the benchBnB project

---
+++ w11d4/benchBnB_part1/app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::Base
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+end
---
+++ w11d4/benchBnB_part1/app/helpers/staticpagescontroller_helper.rb
@@ -0,0 +1,2 @@
+module StaticpagescontrollerHelper
+end
