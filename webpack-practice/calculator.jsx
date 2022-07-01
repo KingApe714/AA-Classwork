1d1ffe48278d2143f06748cd498c26b2d687e97e
KingApe714 <jonathan.diaz714@gmail.com>
2022-07-01 15:23:04 -0400

    functional input fields for the calculator

--- webpack-practice/calculator.jsx
+++ webpack-practice/calculator.jsx
@@ -4,14 +4,41 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     // your code here
+    this.state = {
+        result: 0,
+        num1: '',
+        num2: ''
+    }
+
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
   }
 
   // your code here
 
+  setNum1(e) {
+    e.preventDefault()
+
+    this.setState({ num1: e.target.value })
+  }
+
+  setNum2(e) {
+    e.preventDefault()
+
+    this.setState({ num2: e.target.value })
+  }
+
   render() {
     return (
       <div>
-        <h1>Hello World</h1> // replace this with your code
+        <input  type="text"
+                onChange={this.setNum1}
+                value={this.state.num1} />
+
+        <input  type="text"
+                onChange={this.setNum2}
+                value={this.state.num2} />
+        <h1>{this.state.result}</h1> 
       </div>
     );
   }
