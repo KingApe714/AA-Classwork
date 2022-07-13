ee805ccc81e2e34143eec642ea75874f88a6f983
KingApe714 <jonathan.diaz714@gmail.com>
2022-07-13 17:48:31 -0400

    setting up the chess board

---
+++ js_practice/index.js
@@ -0,0 +1,16 @@
+
+console.log(document.querySelector('.outer-game-container'))
+
+function node(i, j) {
+    this.i = i;
+    this.j = j;
+    this.left = i * 80;
+    this.right = j * 80;
+}
+
+
+for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+
+    }
+}
